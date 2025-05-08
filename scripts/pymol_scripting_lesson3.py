888656740ac9aed74b4e889cc13c060c944ae118
A-Yarrow <yarrowmadrona@gmail.com>
2025-05-08 12:51:50 -0700

    Updated protein_figure() function

---
+++ scripts/pymol_scripting_lesson3.py
@@ -0,0 +1,147 @@
+from pymol import cmd
+import os
+import sys
+
+"""
+Global settings. These can go in setting file
+For simplicity we will keep settings here
+In the future they can go in a settings.py file.
+"""
+#Image Settings
+WIDTH = 800
+HEIGHT = 600
+DPI = 300
+
+#Immediately get the current view for picture taking
+CURRENT_VIEW = cmd.get_view()
+
+#https://pymol.org/dokuwiki/doku.php?id=setting:ray
+def ray_trace():
+
+    cmd.set('ray_trace_mode', 2) #Black and white
+    cmd.set('ray_shadows', 0) #Remove shadows
+    cmd.set('fog', 0) #Remove fog
+    cmd.set('ray_trace_gain', 20) #Create a dark outline
+    cmd.set('ray_trace_slope_factor', 5) #Remove some shadow constrast
+
+def quality(anti_alias: int=4):
+    cmd.set('antialias', anti_alias)#Set the anti-aliasing level to highest available
+
+def protein_figure(protein:str, resi_list:list, image_dir:str, pymol_view:str):
+    """
+    Inputs: Protein as a string, list of selected residues, and 
+    image directory as a string
+    saves a foreground protein image to the image directory
+    Returns the camera/molecule view
+    """
+    #Expand the selection (padded_resi) and covert lists to a string
+    residue_string = "+".join(sorted(str(r) for r in resi_list))
+    min_resi = max(1, resi_list[0]-2)
+    max_resi = resi_list[-1] + 2
+
+    padded_resi = list(range(min_resi, max_resi + 1))
+    expanded_residue_string = "+".join(sorted(str(r) for r in padded_resi))
+    
+    cmd.hide('everything')
+    cmd.show('cartoon', f'{protein} and not resi {residue_string}')
+    cmd.create(f'{protein}_hide', f'{protein} and resi {expanded_residue_string}')
+    cmd.set('ray_opaque_background', 1)
+    cmd.set('bg_rgb', [1,1,1])
+    image = os.path.join(image_dir, f'{protein}_4')
+    cmd.set_view(view)
+    cmd.png(image, width=WIDTH, height=HEIGHT, dpi=DPI, ray=1)
+    print(f"Image saved in {image}")
+    return view
+
+def set_image_dir(image_dir:str=None):
+    """
+    Input: Takes in the image directory as a string
+    Returns: image directory as a string.
+    If no image directory is given look for $HOME/tmp
+    If no $HOME/tmp direcotry exists, create one and return it as
+    the image directory
+    """
+    if image_dir:
+        if os.path.exists(image_dir):
+            return image_dir
+        else: 
+            print(f"The directory: {image_dir} cannot be found, exiting")
+            sys.exit()
+    else:
+        home_dir = os.path.expanduser("~")
+        image_dir = os.path.join(home_dir, 'tmp')
+    
+    if not os.path.exists(image_dir):
+        os.makedirs(image_dir)
+
+    print(f'Image directory set as: {image_dir}')    
+    return image_dir
+
+def get_selection_residues(protein:str, selection_name:str):
+    """
+    Expands the selection to hide and creates an object that overlaps
+    """
+    selection = cmd.get_model(selection_name)
+    residue_set = set()
+    for atom in selection.atom:
+        residue_set.add(int(atom.resi))
+    
+    resi_list = sorted(residue_set)
+    print(f"resi {resi_list}")
+    return resi_list
+
+def select_objects(protein:str, active_site:list):
+    """
+    Checks that the objects can be found in Pymol. 
+    Exits if criteria are not met.
+    Input: Protein string and active_site list
+    Returns: Protein string and active site list
+    """
+    objects = cmd.get_object_list()
+    
+    #check active site objects
+    if set(active_site).issubset(objects):
+        print(f"Found active site molecule objects: {active_site}")
+
+    else:
+        missing = set(active_site) - set(objects)
+        print(f"Unable to find all active site molecules {missing}, exiting")
+        sys.exit()
+
+    #Check protein object
+    if protein in objects:
+        print (f"Found protein molecule object: {protein}")
+        return protein, active_site
+    
+    else:
+        print (f"Protein molecule object {protein} not found, exiting")
+        sys.exit()
+
+
+# ==== Main Execution ====
+#Pymol recieves all arguments as a string so need to parse it.
+def run_selection(arg_string:str, _self=None):
+    """
+    Checks the selection
+    Input: string of protein, ligand, and residues (if entered) 
+    Returns: Protein String, list of active site. Ligand and active site residues
+    """
+    args = arg_string.split()
+    if len(args) < 2 or len(args) > 3:
+        print("Usage: run_selection protein_name ligand residues ....")
+        return
+    protein = args[0]
+    active_sites = args[1:]
+    print(f"Looking for {protein} and {active_sites}", flush=True)
+    
+    protein, active_site = select_objects(protein, active_sites)
+    resi_list = get_selection_residues(protein, selection_name='sele')
+    image_dir = set_image_dir()
+    #Create the background protein figure
+    protein_figure(protein, resi_list, image_dir, CURRENT_VIEW)
+
+   
+cmd.extend("run_selection", run_selection)
+cmd.extend("get_selection_residues", get_selection_residues)
+cmd.extend("set_image_dir", set_image_dir)
+cmd.extend("protein_figure", protein_figure)
